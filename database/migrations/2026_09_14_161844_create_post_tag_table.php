41db247f77ad1e48607fcba46153b0a0da1a5c81
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-14 21:31:00 +0500

    feat: Pivot table using post_id, tag_id foreign keys

--- database/migrations/2026_09_14_161844_create_post_tag_table.php
+++ database/migrations/2026_09_14_161844_create_post_tag_table.php
@@ -12,16 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-
             $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('tag_id');
 
-
             $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
             $table->foreign('tag_id')->references('id')->on('tags')->cascadeOnDelete();
-
-            $table->timestamps();
+            
+            // Composite Primary Key
+            $table->primary(['post_id', 'tag_id']);
         });
     }
 
