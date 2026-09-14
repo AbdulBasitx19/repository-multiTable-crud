6848a96c9a4d42173454b8a4a42d1b661445a76c
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-14 21:49:01 +0500

    feat: added Post interface so we can implement them later in repository

---
+++ app/Interfaces/PostRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Post;
+use \App\Models\Comment;
+use Illuminate\Database\Eloquent\Collection;
+
+interface PostRepositoryInterface
+{
+    public function all(): Collection;
+    public function find(int $id): ?Post;
+    public function create(array $data): Post;
+    public function update(int $id, array $data): Post;
+    public function delete(int $id): bool;
+    public function syncTags(int $postId, array $tagIds): void;
+    public function addComment(int $postId, array $commentData): Comment;
+
+}
