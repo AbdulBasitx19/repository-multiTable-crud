29a27ff10f28a78b72dd9b796f3e777a712b7d47
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-14 21:35:27 +0500

    feat: define fillable and relationships in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     //
+    protected $fillable =[
+        'user_id',
+        'title',
+        'description',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
